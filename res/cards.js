86b86a0527a8a6b723d495b964d7058a5c40978e
tdrumheller <gitlab@dmhlr.org>
2020-01-21 14:49:46 -0700

    json reading

--- res/cards.js
+++ res/cards.js
@@ -1,12 +1,19 @@
-var _ok = 0
-var _skip = 0
+var _ok = 0;
+var _skip = 0;
+var _data = [];
+
+$.getJSON('res/config.json', function( words ){
+  _data = words;
+  console.log(_data)
+});
 
 function ok_click() {
-    _ok++
-    document.getElementById('ok_out').innerHTML = 'OK: ' + _ok.toString() + " times"
+    console.log(_data);
+    _ok++;
+    document.getElementById('ok_out').innerHTML = 'OK: ' + _ok.toString() + " times";
 }
 
 function skip_click() {
-    _skip++
-    document.getElementById('skip_out').innerHTML = 'SKIP: ' + _skip.toString() + " times"
+    _skip++;
+    document.getElementById('skip_out').innerHTML = 'SKIP: ' + _skip.toString() + " times";
 }
