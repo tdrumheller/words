aaa8919e599684ed5c1dd8f5bfd80f8fce4b9deb
tdrumheller <gitlab@dmhlr.org>
2020-01-21 13:31:56 -0700

    wire up intial button capability

--- res/cards.js
+++ res/cards.js
@@ -1,1 +1,12 @@
-alert('Hellow world')
+var _ok = 0
+var _skip = 0
+
+function ok_click() {
+    _ok++
+    document.getElementById('ok_out').innerHTML = 'OK: ' + _ok.toString() + " times"
+}
+
+function skip_click() {
+    _skip++
+    document.getElementById('skip_out').innerHTML = 'SKIP: ' + _skip.toString() + " times"
+}
