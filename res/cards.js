1c37c7d0813373d62967cc64bbf79da7c7cc55fd
tdrumheller <gitlab@dmhlr.org>
2020-01-31 07:43:50 -0700

    clean up syntax

--- res/cards.js
+++ res/cards.js
@@ -10,7 +10,7 @@ var _show_color;
 init();
 
 function init() {
-    $.getJSON('res/config.json', 
+    $.getJSON('res/config.json',
 	      function( words ){
 		  _data = words;
 		  console.log(_data);
@@ -18,26 +18,26 @@ function init() {
 		  create_mode();
 		  create_color_opt();
 	      });
-    start_state()
+    start_state();
 
 }
 
 function ok_click() {
     _ok++;
     document.getElementById('ok_out').innerHTML = 'OK: ' + _ok.toString() + " times";
-    get_next()
+    get_next();
 }
 
 function skip_click() {
     _skip++;
     document.getElementById('skip_out').innerHTML = 'SKIP: ' + _skip.toString() + " times";
-    get_next()
+    get_next();
 }
 
 function create_selectors() {
     var div = document.getElementById('selectors');
     div.appendChild(document.createElement('p').appendChild(document.createTextNode('Word Selection')));
-    add_break('selectors')
+    add_break('selectors');
 
     _all_cb = document.createElement('input');
     _all_cb.type = 'checkbox';
@@ -45,21 +45,20 @@ function create_selectors() {
     _all_cb.value = 'ALL';
     _all_cb.id = 'all_cb';
     _all_cb.checked = true;
-    _all_cb.onchange=all_change
-    
+    _all_cb.onchange=all_change;
+
     var label = document.createElement('label');
     label.htmlFor = 'id';
     label.appendChild(document.createTextNode('All'));
 
     div.appendChild(_all_cb);
     div.appendChild(label);
-    add_break('selectors')
-    
+    add_break('selectors');
+
     for (var i = 0; i < _data.length; i++) {
 	console.log(_data[i]);
 	_cbs.push(create_color_check(_data[i].color));
     }
-    
 }
 
 function create_color_check(color) {
@@ -68,7 +67,7 @@ function create_color_check(color) {
     color_check.name = color + '_cb';
     color_check.value = color;
     color_check.id = color + '_cb';
-    color_check.onchange=update_all_cb
+    color_check.onchange=update_all_cb;
     color_check.checked = true;
 
     var label = document.createElement('label');
@@ -76,25 +75,26 @@ function create_color_check(color) {
     label.appendChild(document.createTextNode(color));
 
     var div = document.getElementById('selectors');
-    
     div.appendChild(color_check);
     div.appendChild(label);
-    add_break('selectors')
+    add_break('selectors');
 
     return color_check;
 }
 
 function add_break(id) {
     var br = document.createElement('br');
-    document.getElementById(id).appendChild(br)
+    document.getElementById(id).appendChild(br);
 }
 
 function all_change() {
     if(_all_cb.checked) {
-	_cbs.forEach( 
+	_cbs.forEach(
 	    function(item, index) {
 		item.checked = true;
-	    })}
+	    }
+	);
+    }
 }
 
 function update_all_cb() {
@@ -104,78 +104,78 @@ function update_all_cb() {
 	    if (!item.checked) {
 		all_checked = false;
 	    }
-	})
-    _all_cb.checked = all_checked
+	});
+    _all_cb.checked = all_checked;
 }
 
 function create_mode() {
-    var div = document.getElementById('mode')
+    var div = document.getElementById('mode');
 
     div.appendChild(document.createElement('p').appendChild(document.createTextNode('Exercise type')));
-    add_break('mode')
+    add_break('mode');
 
     var mode = document.createElement('input');
     mode.type='radio';
     mode.id='mode_cont';
-    mode.name='mode'
+    mode.name='mode';
     mode.value='continuous';
-    mode.checked = 'true'
-    mode.onchange=eval_mode
-    _mode = 'continuous'
+    mode.checked = 'true';
+    mode.onchange=eval_mode;
+    _mode = 'continuous';
     var label = document.createElement('label');
     label.htmlFor = 'id';
     label.appendChild(document.createTextNode('Continuous'));
-    div.appendChild(mode)
-    div.appendChild(label)
-    add_break('mode')
+    div.appendChild(mode);
+    div.appendChild(label);
+    add_break('mode');
 
     mode = document.createElement('input');
     mode.type='radio';
     mode.id='mode_once';
-    mode.name='mode'
+    mode.name='mode';
     mode.value='once_through';
-    mode.onchange=eval_mode
+    mode.onchange=eval_mode;
     var label = document.createElement('label');
     label.htmlFor = 'id';
     label.appendChild(document.createTextNode('Once Through'));
-    div.appendChild(mode)
-    div.appendChild(label)
-    add_break('mode')
+    div.appendChild(mode);
+    div.appendChild(label);
+    add_break('mode');
 }
 
 function create_color_opt() {
-    var div = document.getElementById('show_colors')
+    var div = document.getElementById('show_colors');
 
     div.appendChild(document.createElement('p').appendChild(document.createTextNode('Show card colors')));
-    add_break('show_colors')
+    add_break('show_colors');
 
     var but = document.createElement('input');
     but.type='radio';
     but.id='show_color';
-    but.name='show_color'
+    but.name='show_color';
     but.value=true;
-    but.checked = 'true'
-    but.onchange=eval_color
-    _show_color = true
+    but.checked = 'true';
+    but.onchange=eval_color;
+    _show_color = true;
     var label = document.createElement('label');
     label.htmlFor = 'id';
     label.appendChild(document.createTextNode('Show Colors'));
-    div.appendChild(but)
-    div.appendChild(label)
-    add_break('show_colors')
+    div.appendChild(but);
+    div.appendChild(label);
+    add_break('show_colors');
 
     but = document.createElement('input');
     but.type='radio';
     but.id='no_color';
-    but.name='show_color'
+    but.name='show_color';
     but.value=false;
-    but.onchange=eval_color
+    but.onchange=eval_color;
     var label = document.createElement('label');
     label.htmlFor = 'id';
     label.appendChild(document.createTextNode('No Card Color'));
-    div.appendChild(but)
-    div.appendChild(label)
-    add_break('show_colors')
+    div.appendChild(but);
+    div.appendChild(label);
+    add_break('show_colors');
 }
 
 
